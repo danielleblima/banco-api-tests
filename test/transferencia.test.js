a9587ffbf2f64d5deb8b4d6175a6b7550c3d7018
danielleblima.ti@gmail.com <danielleblima.ti@gmail.com>
2026-04-08 15:10:38 -0300

    Adicionando a biblioteca dotenv para variaveis globais

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest');
 const { expect } = require('chai');
+require('dotenv').config()
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de 10,00 reais', async () => {
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -14,7 +15,7 @@ describe('Transferencias', () => {
 
                 const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                .post('/transferencias')
                .set('Content-Type', 'application/json')
                .set('Authorization', `Bearer ${token}`)
@@ -32,7 +33,7 @@ describe('Transferencias', () => {
         })
 
         it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de 10,00 reais', async () => {
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -42,7 +43,7 @@ describe('Transferencias', () => {
 
                 const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                .post('/transferencias')
                .set('Content-Type', 'application/json')
                .set('Authorization', `Bearer ${token}`)
